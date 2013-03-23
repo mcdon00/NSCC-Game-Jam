636386921b54f1f0e8fad23a76ced0958e7a6f12
Nathan McDonald <nmcdonald00@gmail.com>
2013-03-23 17:52:41 -0300

    added enemy functionalty, enemey now follows character when within a certain range

--- classes/Enemy.js
+++ classes/Enemy.js
@@ -1,7 +1,8 @@
 function Enemy(myStage){
 	//constants
-	var HEALTH = 100;
+	var HEALTH = 800;
 	var SPEED = 7;
+	var DISTANCE_TO_ATTACK = 100;
 
 	//------------------------------contructor
 	this.init = function(myStage){
@@ -9,63 +10,71 @@ function Enemy(myStage){
 		this.speed = SPEED;
 		this.stage = myStage
 		this.clip = new createjs.Shape();
-		this.clip.graphics.beginStroke("white").drawRect(erx,ery,ex,ey);
+		this.clip.graphics.beginStroke("white").drawCircle(0,0,4);
 		this.clip.y = window.innerHeight/3;
 		this.clip.x = window.innerWidth/3;
-		this.vx = 0;
-		this.vy = 0;
+		this.vx = 1;
+		this.vy = 1;
 		this.health = HEALTH;
 		this.isAttack = false;
 		stage.addChild(this.clip);
 	}
 	//------------------------------public methods
 	this.checkPlayerPos = function(playerX,playerY){
-		var distanceX = Math.abs(this.clip.x - playerX);
-		var distanceY = Math.abs(this.clip.y - playerY);
 
-		var targetX = playerX;
-		var targetY = playerY;
-		var hy = 0;
-		isXNeg = false;
-		isYNeg = false;
+		distanceX = Math.abs((this.clip.x - playerX));
+		distanceY = Math.abs((this.clip.y - playerY));
 
-		var bX = this.clip.x;
-		var bY = this.clip.y;
-		
-		if(playerX <= bX)isXNeg = true;
-		if(playerY <= bY)isYNeg = true;
+		distance = Math.sqrt((distanceX*distanceX) + (distanceY*distanceY));
+		// hy -= (obj1.r + obj2.r);
+			var targetX = 0;
+			var targetY = 0;
+			var hy = 0;
+			isXNeg = false;
+			isYNeg = false;
+
+			var bX = this.clip.x;
+			var bY = this.clip.y;
 			
-		targetY = Math.abs(mouse.y - bY);
-		targetX = Math.abs(mouse.x - bX);
+			if(playerX <= bX)isXNeg = true;
+			if(playerY <= bY)isYNeg = true;
+				
+			targetY = Math.abs(playerY - bY);
+			targetX = Math.abs(playerX - bX);
 
-		hy = Math.sqrt((targetY * targetY) + (targetX * targetX));
+			hy = Math.sqrt((targetY * targetY) + (targetX * targetX));
+
+			if (!isXNeg && !isYNeg) {
+				this.vx = targetX/hy;
+				this.vy = targetY/hy;
+			}else if (isXNeg && isYNeg) {
+				this.vx = (targetX/hy)*-1;
+				this.vy = (targetY/hy)*-1;
+			}else if(!isXNeg && isYNeg){
+				this.vx = targetX/hy;
+				this.vy = (targetY/hy)*-1;
+			}else if(isXNeg && !isYNeg){
+				this.vx = (targetX/hy)*-1;
+				this.vy = targetY/hy;
+			}
+			
+			this.vx = this.vx *2; 
+			this.vy = this.vy *2; 
 		
-
-
-
-
-		// if (!isXNeg && !isYNeg) {
-		// 	this.vx = targetX/hy;
-		// 	this.vy = targetY/hy;
-		// }else if (isXNeg && isYNeg) {
-		// 	this.vx = (targetX/hy)*-1;
-		// 	this.vy = (targetY/hy)*-1;
-		// }else if(!isXNeg && isYNeg){
-		// 	this.vx = targetX/hy;
-		// 	this.vy = (targetY/hy)*-1;
-		// }else if(isXNeg && !isYNeg){
-		// 	this.vx = (targetX/hy)*-1;
-		// 	this.vy = targetY/hy;
-		// }
-
-		
-		// this.vx = this.vx *2; 
-		// this.vy = this.vy *2; 
-
-
-		// if(){
-		// 	this.isAttack = true;
-		// }
+		if(distance <= DISTANCE_TO_ATTACK){
+			this.isAttack = true;
+			this.move();
+		}else{
+			this.isAttack = false;
+		}
 	}
+
+	this.move = function(){
+		if(this.isAttack){
+			this.clip.x += this.vx;
+			this.clip.y += this.vy;
+		}	
+	}
+	
 
 }
