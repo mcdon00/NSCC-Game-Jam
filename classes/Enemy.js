5543ba21f3c1c5954581f3b92b2aed7a59b1da00
Nathan McDonald <nmcdonald00@gmail.com>
2013-03-24 05:19:01 -0300

    created sprite animation for enemy and randomly generated locations

--- classes/Enemy.js
+++ classes/Enemy.js
@@ -2,29 +2,36 @@ function Enemy(myStage){
 	//constants
 	var HEALTH = 800;
 	var SPEED = 7;
-	var DISTANCE_TO_ATTACK = 100;
+	var DISTANCE_TO_ATTACK = 300;
 
 	//------------------------------contructor
-	this.init = function(myStage){
+	this.init = function(myStage,myX,myY){
 	
 		this.speed = SPEED;
 		this.stage = myStage
 		this.type="";
 		var ss = new createjs.SpriteSheet({ "animations":{
-	        "stand":[0, 0]},
-	        "images":["lib/character_sheet.png"],
+	    	"moveLeft":[11, 12],
+	    	"moveDownRight":[10,13],
+	    	"moveRight":[15,19],
+	    	"moveDownLeft":[3,6],
+	    	"moveUpRight":[5,6],
+	    	"moveUpLeft":[9,10],
+	    	"moveUp":[19,22],
+	    	"moveDown":[0,0]},
+	        "images":["lib/garbageBag.png"],
 	        "frames":{
-	            "regX":25,
-	            "regY":32.5,
-	            "height":65,
-	            "width":50,
-	            "count":3
+	            "regX":59,
+	            "regY":52,
+	            "height":104,
+	            "width":119,
+	            "count":23
 	        }
     	});
     	this.clip = new createjs.BitmapAnimation(ss);
-    	this.clip.gotoAndPlay("stand");
-		this.clip.y = window.innerHeight/3;
-		this.clip.x = window.innerWidth/3;
+    	this.clip.play();
+		this.clip.y = myY;
+		this.clip.x = myX;
 		this.vx = 1;
 		this.vy = 1;
 		this.health = HEALTH;
@@ -59,15 +66,22 @@ function Enemy(myStage){
 			if (!isXNeg && !isYNeg) {
 				this.vx = targetX/hy;
 				this.vy = targetY/hy;
+				// if(!(this.clip.currentAnimation == "moveUpLeft")){
+						
+				// }
+				
 			}else if (isXNeg && isYNeg) {
 				this.vx = (targetX/hy)*-1;
 				this.vy = (targetY/hy)*-1;
+				//this.clip.gotoAndPlay("moveDownRight");
 			}else if(!isXNeg && isYNeg){
 				this.vx = targetX/hy;
 				this.vy = (targetY/hy)*-1;
+				
 			}else if(isXNeg && !isYNeg){
 				this.vx = (targetX/hy)*-1;
 				this.vy = targetY/hy;
+				//this.clip.gotoAndPlay("moveDownRight");
 			}
 			
 			this.vx = this.vx *4; 
@@ -83,6 +97,27 @@ function Enemy(myStage){
 
 	this.move = function(){
 		if(this.isAttack){
+			if(isXNeg){
+				if(isYNeg){
+					if(!(this.clip.currentAnimation == "moveUp")){
+						this.clip.gotoAndPlay("moveUp");	
+					}
+					
+				}else{
+					if(!(this.clip.currentAnimation == "moveDownLeft")){
+						this.clip.gotoAndPlay("moveDownLeft");
+					}
+				}
+			}else if(isYNeg){
+				if(!(this.clip.currentAnimation == "moveUp")){
+					this.clip.gotoAndPlay("moveUp");	
+				}
+				//entPlayer.graphic = sprWalkBackRight;
+			}else{
+				if(!(this.clip.currentAnimation == "moveDownRight")){
+					this.clip.gotoAndPlay("moveDownRight");	
+				}
+			}
 			this.clip.x += this.vx;
 			this.clip.y += this.vy;
 		}	
